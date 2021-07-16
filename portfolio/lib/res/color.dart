4deefc62b9cbc5594593dff5226fa33d5a1a52e5
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-07-16 18:28:01 +0300

    INIT

---
+++ portfolio/lib/res/color.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  //  melon
+  static Color melonBackground = Color(0XCCF1B39C);
+  static Color melonFontcolor = Color(0XFFDD602A);
+  //  baby Pink
+  static Color babyPinkBackground = Color(0XCCF4CBC5);
+  static Color babyPinkFontcolor = Color(0XFFF13641);
+  //  right Navy Blue
+  static Color brightNavyBlueBackground = Color(0XCCB2C6DE);
+  static Color brightNavyBlueFontcolor = Color(0XFFF13641);
+  //  lavender  Blue
+  static Color lavenderBlueBackground = Color(0XCCC0C3E6);
+  static Color lavenderBlueFontcolor = Color(0XFF553AD7);
+  //   Deep Champagne
+  static Color deepChampagneBackground = Color(0XCCC0C3E6);
+  static Color deepChampagneFontcolor = Color(0XFFDD9D17);
+  //   Opal  Blue
+  static Color opalBackground = Color(0XCCB6D0C7);
+  static Color opalFontcolor = Color(0XFF008373);
+  //   background
+  static Color background = Colors.white;
+}
