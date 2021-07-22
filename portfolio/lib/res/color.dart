c3e7d7e2602c9181ff4a2f39444e26468282efa4
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-07-22 14:18:47 +0300

    fix color and make list color (back & fore)

--- portfolio/lib/res/color.dart
+++ portfolio/lib/res/color.dart
@@ -9,16 +9,33 @@ class AppColor {
   static Color babyPinkFontcolor = Color(0XFFF13641);
   //  right Navy Blue
   static Color brightNavyBlueBackground = Color(0XCCB2C6DE);
-  static Color brightNavyBlueFontcolor = Color(0XFFF13641);
+  static Color brightNavyBlueFontcolor = Color(0XFF2778E3);
   //  lavender  Blue
   static Color lavenderBlueBackground = Color(0XCCC0C3E6);
   static Color lavenderBlueFontcolor = Color(0XFF553AD7);
   //   Deep Champagne
-  static Color deepChampagneBackground = Color(0XCCC0C3E6);
+  static Color deepChampagneBackground = Color(0XCCF9D38A);
   static Color deepChampagneFontcolor = Color(0XFFDD9D17);
   //   Opal  Blue
   static Color opalBackground = Color(0XCCB6D0C7);
   static Color opalFontcolor = Color(0XFF008373);
   //   background
   static Color background = Colors.white;
+
+  static List<Color> listBackground = [
+    Color(0XCCF1B39C),
+    Color(0XCCB6D0C7),
+    Color(0XCCF4CBC5),
+    Color(0XCCB2C6DE),
+    Color(0XCCC0C3E6),
+    Color(0XCCF9D38A),
+  ];
+  static List<Color> listForeground = [
+    Color(0XFFDD602A),
+    Color(0XFF008373),
+    Color(0XFFF13641),
+    Color(0XFF2778E3),
+    Color(0XFF553AD7),
+    Color(0XFFDD9D17),
+  ];
 }
