ce9fc485013834b18b0451f9083564f117bebaea
akramAssi <55202260+akramAssi@users.noreply.github.com>
2021-07-22 14:20:28 +0300

    add project model

---
+++ portfolio/lib/model/project.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Project {
+  List<String> images;
+  String name;
+  List<String> technologyStack;
+  Project(
+      {@required this.images,
+      @required this.name,
+      @required this.technologyStack});
+}
